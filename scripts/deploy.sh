ec2fe67adcb7521386d9a3295bcda7f05d45ccca
fanzru <fattahilaaf080701@gmail.com>
2022-03-19 00:33:40 +0700

    update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,7 @@ echo "Update Codebase"
 git fetch origin development
 git reset --hard origin/development
 
-echo "Installing dependencies 🛠"
-go mod tidy
+
 
 echo "Golang Set Up"
 cd 
@@ -20,6 +19,9 @@ source .profile
 # pm2 restart deploy.json
 echo "to Directory File"
 cd /home/fanzru/backend/BE-SISFO-KLINIK/
+
+echo "Installing dependencies 🛠"
+go mod tidy
 
 echo "Restart pm2 service 🔥"
 pm2 restart deploy.json
