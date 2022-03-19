906a38a0fce147fc3ab05872615f7ee4932c1435
fanzru <fattahilaaf080701@gmail.com>
2022-03-19 18:52:37 +0700

    fix bug edit and aktifasi

--- repositories/pasienRepo.go
+++ repositories/pasienRepo.go
@@ -72,7 +72,7 @@ func ActivatePasienDB(id int64) (models.Pasien, error, bool) {
 		return pasien, err, false
 	}
 
-	result := db.Model(&pasien).Where("id_pemeriksaan = ? AND delete_status = 1", id).Updates(map[string]interface{}{"delete_status": false, "updated_at": time.Now()})
+	result := db.Model(&pasien).Where("id = ? AND delete_status = 1", id).Updates(map[string]interface{}{"delete_status": false, "updated_at": time.Now()})
 	if result.Error != nil {
 		return pasien, result.Error, false
 	} else if result.RowsAffected == 0 {
@@ -108,7 +108,7 @@ func EditPasienDB(data *models.Pasien) (*models.Pasien, bool, error) {
 		"jenis_kelamin":        data.JenisKelamin,
 		"updated_at":           time.Now(),
 	}
-	result := db.Model(&data).Where("id_pemeriksaan = ? AND delete_status = 0", data.Id).Updates(updateData)
+	result := db.Model(&data).Where("id = ? AND delete_status = 0", data.Id).Updates(updateData)
 	if result.Error != nil {
 		return data, false, result.Error
 	} else if result.RowsAffected == 0 {
