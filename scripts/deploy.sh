f62d43c0c7235fa72c7e55c210cb506b9873c6ea
fanzru <fattahilaaf080701@gmail.com>
2022-03-19 18:54:32 +0700

    fix bug edit and aktifasi

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,18 +4,18 @@ cd ..
 echo "Deploying application ..."
 
 # Update codebase
-echo "Update Codebase"
+echo "Update Codebase....."
 git fetch origin development
 git reset --hard origin/development
 
-echo "Golang Set Up"
+echo "Golang Set Up 🔥"
 cd
 source .profile
 
 # source /home/fanzru/.profile
 # echo "Restart pm2 service 🔥"
 # pm2 restart deploy.json
-echo "to Directory File"
+echo "to Directory File 🔥"
 cd /home/fanzru/backend/BE-SISFO-KLINIK/
 
 echo "Installing dependencies 🛠"
